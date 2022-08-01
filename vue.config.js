8f3974b630106ef94131131d3b6b82fe152cacb8
xhy <2987868039@qq.com>
2022-08-01 12:04:15 +0800

    three

--- vue.config.js
+++ vue.config.js
@@ -1,7 +1,3 @@
-const {
-	defineConfig
-} = require('@vue/cli-service')
-module.exports = defineConfig({
-	publicPath: '.',
-	transpileDependencies: true
-})
+module.exports = {
+	publicPath: '.'
+}
